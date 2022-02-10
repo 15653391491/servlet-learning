d1a19f8f4ffe7de2cdb7c41cc03a5057a221b46c
C1810 <gybond1995@foxmail.com>
2022-02-10 17:36:29 +0800

    新建servlet成功

--- project/servlet/src/main/java/Servlet.java
+++ project/servlet/src/main/java/Servlet.java
@@ -2,13 +2,20 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Enumeration<String> names = req.getParameterNames();
+        while (names.hasMoreElements()) {
+            String name = (String) names.nextElement();
+            System.out.println(req.getParameter(name));
+        }
         PrintWriter writer = resp.getWriter();
-        writer.println("111111");
+        writer.println("1111222211");
     }
 }
