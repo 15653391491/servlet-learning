968da48f743df76203a473bad246b8f6c08b5096
C1810 <gybond1995@foxmail.com>
2022-02-11 09:55:56 +0800

    servlet 返回json测试

--- project/servlet/src/main/java/Servlet.java
+++ project/servlet/src/main/java/Servlet.java
@@ -6,16 +6,31 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
 
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        ------------- 接收 -------------------
         Enumeration<String> names = req.getParameterNames();
+        Map<String, String> params = new HashMap<>();
         while (names.hasMoreElements()) {
             String name = (String) names.nextElement();
-            System.out.println(req.getParameter(name));
+            params.put(name, req.getParameter(name));
         }
+//        ------------ 验证 ----------------
+//        ------------ 处理 ----------------
+        Map<String, Object> con = new HashMap<>();
+        con.put("data", params);
+        con.put("code", 1);
+        con.put("msg", "ok");
+//        ------------ 返回 ----------------
+        resp.setContentType("text/json; charset=utf-8");
+        JSONObject content = new JSONObject(con);
         PrintWriter writer = resp.getWriter();
-        writer.println("1111222211");
+        writer.println(content);
     }
 }
